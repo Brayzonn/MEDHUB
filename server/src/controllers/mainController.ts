dc68f9e9a4efc466c4d57bb8ee20d6dd45cabbb6
Brayzonn <brayzonn@gmail.com>
2024-02-18 17:44:45 +0100

    Delete server directory

--- server/src/controllers/mainController.ts
+++
@@ -1,6 +0,0 @@
-import signInFnc from '../config/signinLogic';
-import { Request, Response, NextFunction } from 'express';
-import generateUserToken from '../config/jwt';
-
-require('dotenv').config();
-
--- server/src/middleware/corsMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import cors, { CorsOptions } from 'cors';
-
-const corsOptions: CorsOptions = {
-  origin: ['http://localhost:3000'],
-  credentials: true
-};
-
-export default cors(corsOptions);
