7adff0fe4ea69b5e9d7c053d3933d0bd825f27ce
Brayzonn <brayzonn@gmail.com>
2025-04-27 12:15:30 +0100

    bug fixes in dropdown

--- client/src/components/globalComponents/DropDownList.tsx
+++ client/src/components/globalComponents/DropDownList.tsx
@@ -1,7 +1,7 @@
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { useState } from "react";
 
-import {AddDoctorFormInterface, AddPatientFormInterface, NurseFormProps, StaffFormProps} from '../DataTypes'
+import {AddDoctorFormInterface, AddPatientFormInterface} from '../DataTypes'
 
     //doctor dropdown
     interface dropdownContainer {
@@ -137,121 +137,5 @@ import {AddDoctorFormInterface, AddPatientFormInterface, NurseFormProps, StaffFo
 
 
 
-    // add nurse drop down
-    interface dropDownPropsNurse {
-            allNurseDropDownContainer: dropdownContainer[],
-            setNurseSubmitFormDropdown: React.Dispatch<React.SetStateAction<NurseFormProps>>,
-    }
-
-
-    const DropDownListNurse: React.FC<dropDownPropsNurse> = ({ setNurseSubmitFormDropdown, allNurseDropDownContainer }) => {
-
-        const [nurseDropDownStates, updateNurseDropDownStates] = useState<number[]>(Array(allNurseDropDownContainer.length).fill(-1));
-        const [test, uptest] = useState<{ [key: string]: string }>({});
-
-        return (
-            <>
-            {allNurseDropDownContainer.map((dropdownFeature: dropdownContainer, index: number) => (
-                <div className="relative flex items-end space-y-1" key={index}>
-                        <button
-                                onClick={() => {
-                                    const newStates = [...nurseDropDownStates];
-                                    newStates[index] = newStates[index] === -1 ? index : -1;
-                                    updateNurseDropDownStates(newStates);
-                                }}
-                                className="shadow-inner p-2 border-[#e1e1e1] border-[1px] rounded-[5px] w-[200px] min-h-[42px] text-black bg-white flex items-center justify-between"
-                        >
-                                <p className="text-[#636363]">
-                                    {nurseDropDownStates[index] !== -1 ? (test[dropdownFeature.buttonName] === undefined ? dropdownFeature.buttonName : test[dropdownFeature.buttonName]) : test[dropdownFeature.buttonName]}
-                                </p>
-                                {nurseDropDownStates[index] !== -1 ? <FaChevronUp className="text-[#636363]" /> : < FaChevronDown className="text-[#636363]" />}
-                        </button>
-
-                        {nurseDropDownStates[index] !== -1 && (
-                                <ul className="absolute z-50 left-0 p-2 overflow-y-auto flex flex-col space-y-1 top-[100%] text-[14px] w-[200px] max-h-[550px] bg-black border border-black border-r-[#fff] rounded-[5px] text-white">
-                                        {dropdownFeature.listOptions.map((objectData: string, optionIndex: number, array: string[]) => (
-                                            <li
-                                                key={optionIndex}
-                                                onClick={() => {
-                                                    const newTest = { ...test };
-                                                    newTest[dropdownFeature.buttonName] = objectData;
-                                                    uptest(newTest);
-                                                    setNurseSubmitFormDropdown((prevForm: NurseFormProps) => ({ ...prevForm, [dropdownFeature.buttonId]: objectData }));
-                                                    const newStates = [...nurseDropDownStates];
-                                                    newStates[index] = -1;
-                                                    updateNurseDropDownStates(newStates);
-                                                }}
-                                                className={`${optionIndex !== array.length - 1 && 'border-b border-b-[#3e3e3e]'} py-1 cursor-pointer`}
-                                            >
-                                                {objectData}
-                                            </li>
-                                        ))}
-                                </ul>
-                        )}
-                </div>
-            ))}
-            </>
-        );
-    };
-
-
-
-
-    // add staff drop down
-    interface dropDownPropsStaff {
-            allStaffDropDownContainer: dropdownContainer[],
-            setStaffSubmitFormDropdown: React.Dispatch<React.SetStateAction<StaffFormProps>>,
-    }
-
-
-    const DropDownListStaff: React.FC<dropDownPropsStaff> = ({ setStaffSubmitFormDropdown, allStaffDropDownContainer }) => {
-
-        const [staffDropDownStates, updateStaffDropDownStates] = useState<number[]>(Array(allStaffDropDownContainer.length).fill(-1));
-        const [test, uptest] = useState<{ [key: string]: string }>({});
-
-        return (
-            <>
-            {allStaffDropDownContainer.map((dropdownFeature: dropdownContainer, index: number) => (
-                <div className="relative flex items-end space-y-1" key={index}>
-                        <button
-                                onClick={() => {
-                                    const newStates = [...staffDropDownStates];
-                                    newStates[index] = newStates[index] === -1 ? index : -1;
-                                    updateStaffDropDownStates(newStates);
-                                }}
-                                className="shadow-inner p-2 border-[#e1e1e1] border-[1px] rounded-[5px] w-[200px] min-h-[42px] text-black bg-white flex items-center justify-between"
-                        >
-                                <p className="text-[#636363]">
-                                    {staffDropDownStates[index] !== -1 ? (test[dropdownFeature.buttonName] === undefined ? dropdownFeature.buttonName : test[dropdownFeature.buttonName]) : test[dropdownFeature.buttonName]}
-                                </p>
-                                {staffDropDownStates[index] !== -1 ? <FaChevronUp className="text-[#636363]" /> : < FaChevronDown className="text-[#636363]" />}
-                        </button>
-
-                        {staffDropDownStates[index] !== -1 && (
-                                <ul className="absolute z-50 left-0 p-2 overflow-y-auto flex flex-col space-y-1 top-[100%] text-[14px] w-[200px] max-h-[550px] bg-black border border-black border-r-[#fff] rounded-[5px] text-white">
-                                        {dropdownFeature.listOptions.map((objectData: string, optionIndex: number, array: string[]) => (
-                                            <li
-                                                key={optionIndex}
-                                                onClick={() => {
-                                                    const newTest = { ...test };
-                                                    newTest[dropdownFeature.buttonName] = objectData;
-                                                    uptest(newTest);
-                                                    setStaffSubmitFormDropdown((prevForm: StaffFormProps) => ({ ...prevForm, [dropdownFeature.buttonId]: objectData }));
-                                                    const newStates = [...staffDropDownStates];
-                                                    newStates[index] = -1;
-                                                    updateStaffDropDownStates(newStates);
-                                                }}
-                                                className={`${optionIndex !== array.length - 1 && 'border-b border-b-[#3e3e3e]'} py-1 cursor-pointer`}
-                                            >
-                                                {objectData}
-                                            </li>
-                                        ))}
-                                </ul>
-                        )}
-                </div>
-            ))}
-            </>
-        );
-    };
-
-export {PatientDropDownList, DoctorDropDownList, DropDownListNurse, DropDownListStaff} 
+  
+export {PatientDropDownList, DoctorDropDownList} 
