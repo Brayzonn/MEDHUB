4f496dd210d5660cd3a1912c213f6e672d9a8057
Brayzonn <brayzonn@gmail.com>
2025-04-27 23:45:09 +0100

    server bcrypt to bcryptjs fix

--- server/src/controllers/mainController.ts
+++ server/src/controllers/mainController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import path from 'path';
 import generateUserToken from '../utils/jwtTokenFunc';
 import multer from 'multer';
