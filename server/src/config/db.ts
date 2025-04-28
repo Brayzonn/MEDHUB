e93ebdf42ea26620b9750d545163dcb0253c695a
Brayzonn <brayzonn@gmail.com>
2025-04-28 04:57:48 +0100

    mongo config update to attempt to fix connection timeout issues

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,19 +1,25 @@
 import mongoose from 'mongoose';
-require('dotenv').config();
+import dotenv from 'dotenv';
 
-const mongoURI: string | undefined = process.env.MongoURI;
+dotenv.config();
 
-//connect to Mongo
-async function connectToDatabase() {
-      try {
-            await mongoose.connect(mongoURI ? mongoURI : '', {
-                  useNewUrlParser: true,
-                  useUnifiedTopology: true,
-            } as mongoose.ConnectOptions);
-                  console.log('MongoDB connected.....');
-      } catch (error) {
-                  console.error('MongoDB connection error:', error);
-      }
+const mongoURI = process.env.MongoURI;
+
+async function connectToDatabase(): Promise<void> {
+  if (!mongoURI) {
+    console.error('MongoURI not found in environment variables.');
+    return;
   }
 
-export default connectToDatabase
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions);
+    console.log('MongoDB connected...');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
+
+export default connectToDatabase;
