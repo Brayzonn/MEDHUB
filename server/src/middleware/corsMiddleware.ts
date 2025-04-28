810e56aa4f7461316d2b82e9874a66acf01c94a7
Brayzonn <brayzonn@gmail.com>
2025-04-28 03:18:52 +0100

    proxy/baseURL ADAPTABILITY FOR OTHER ENVS

--- server/src/middleware/corsMiddleware.ts
+++ server/src/middleware/corsMiddleware.ts
@@ -1,7 +1,7 @@
 import cors, { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-    origin: ["http://localhost:3000",  "http://localhost:4173", "https://med-hub-hazel.vercel.app/"],
+    origin: ["http://localhost:3000", "https://med-hub-hazel.vercel.app"],
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
