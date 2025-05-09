e6966eeb61a43e4f8d3dbcc670de61e791317f44
Brayzonn <brayzonn@gmail.com>
2025-05-09 15:02:16 +0100

    cors logic update to accomodate origins in .env

--- server/src/middleware/corsMiddleware.ts
+++ server/src/middleware/corsMiddleware.ts
@@ -1,8 +1,19 @@
 import cors, { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',').map(origin => origin.trim()) || [];
 
 const corsOptions: CorsOptions = {
-    origin: ["http://localhost:3000", "https://med-hub-hazel.vercel.app"],
-    credentials: true, 
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 };
 
 export default cors(corsOptions);
+
